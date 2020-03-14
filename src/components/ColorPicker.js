7878b444a6c7dde888019c566d830e51832b6134
Joseph Pulaski <jpulaski79@gmail.com>
2020-03-14 16:43:42 -0400

    colorPicker props added

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,18 +1,46 @@
 //Dependency 
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+
 
 // ColorPicker Component
-const ColorPicker = () => {
-    return (
-        <View>
-            <Text>Color Picker Component</Text>
-        </View>
-    
-    )
-}
+const ColorPicker = ({ titleText, onPressTop, onPressBottom, bottomTitle, topTitle }) => {
+    return <View style={styles.wrapper}>
+        <Text>{titleText}</Text>
+        <TouchableOpacity style={styles.button} onPress={onPressTop}>
+            <Text style={styles.text}>{topTitle}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={onPressBottom}>
+            <Text style={styles.text}>{bottomTitle}</Text>
+        </TouchableOpacity>
+  </View>
+};
+
+
 //style sheet
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrapper: {
+    alignContent: 'center',
+    marginHorizontal: 100,
+    marginTop: 25,
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: "helvetica",    
+    fontWeight: "bold",
+
+
+  },
+  button: {
+    borderColor: 'blue',
+    borderWidth: 3,
+    marginTop: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+  },
+});
 
 //export
 export default ColorPicker
--- src/screens/ColorPickerScreen.js
+++ src/screens/ColorPickerScreen.js
@@ -4,17 +4,23 @@ import { StyleSheet, Text, View } from 'react-native'
 import ColorPicker from '../components/ColorPicker'
 
 // ColorPickerScreen Component
-const ColorPickerScreen = () => {
+const ColorPickerScreen = props => {
     return (
         <View>
-            <Text>Color Picker Screen</Text>
-            <ColorPicker/>
+            <Text style={styles.headerText}>Color Picker Screen</Text>
+            <ColorPicker titleText="Red Scale" topTitle="+ Red" bottomTitle="- Red" onPressTop={() => {console.log('RED ++')}} onPressBottom={() => {console.log('RED --')}} />
+            <ColorPicker titleText="Green Scale" topTitle="+ Green" bottomTitle="- Green" onPressTop={() => {console.log('Green ++')}} onPressBottom={() => {console.log('Green --')}} />
+            <ColorPicker titleText="Blue Scale" topTitle="+ Blue" bottomTitle="- Blue" onPressTop={() => {console.log('Blue ++')}} onPressBottom={() => {console.log('Blue --')}} />
         </View>
     )
 }
 
 //style sheet
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headerText: {
+        alignSelf: 'center',
+    }
+})
 
 //export
 export default ColorPickerScreen
