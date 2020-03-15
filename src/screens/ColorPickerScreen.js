c0c09bf662531c3611418cc0bb8bdbe7db5a8602
Joseph Pulaski <jpulaski79@gmail.com>
2020-03-14 21:38:16 -0400

    colorValueController

--- src/screens/ColorPickerScreen.js
+++ src/screens/ColorPickerScreen.js
@@ -3,47 +3,86 @@ import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import ColorPicker from '../components/ColorPicker'
 
+
+const COLOR_CHANGE = 15;
+
 // ColorPickerScreen Component
 const ColorPickerScreen = () => {
 
     //initial functional state 
-    const [redIntensity, setRedIntensity] = useState(0)
-    const [greenIntensity, setGreenIntensity] = useState(0)
-    const [blueIntensity, setBlueIntensity] = useState(0)
+    const [red, setRedIntensity] = useState(0)
+    const [green, setGreenIntensity] = useState(0)
+    const [blue, setBlueIntensity] = useState(0)
 
-    //logs
-    console.log(`RED INTENSITY: ${redIntensity}`)
-    console.log(`GREEN INTENSITY: ${greenIntensity}`)
-    console.log(`BLUE INTENSITY: ${blueIntensity}`)
- 
+    // logs
+    console.log(red)
+    console.log(green)
+    console.log(blue)
+
+    
+    //rgb
+    let color = `${red},${green},${blue}`;
+
+    //color value control function 
+    const colorValueController = (colorVal, ColorValChange) => {
+        switch(colorVal) {
+            case 'Red':
+                if (red + ColorValChange <= -1 || red + ColorValChange >= 256) {
+                    console.log("RED CASE HIT: " , red);
+                    return;
+                } else {
+                    setRedIntensity(red + ColorValChange);
+                }
+                return;
+            case 'Green':
+                if (green + ColorValChange <= -1 ||green + ColorValChange >= 256) {
+                    console.log("GREEN CASE HIT: " , green);
+                    return;
+                } else {
+                    setGreenIntensity(green + ColorValChange);
+                }
+                return;                
+            case 'Blue':
+                if (blue + ColorValChange <= -1 || blue + ColorValChange >= 256) {
+                    console.log("BLUE CASE HIT: " , blue);
+                    return;
+                } else {
+                    setBlueIntensity(blue + ColorValChange);
+                }
+                return;
+            }
+    }
+
     // colorPickers for RBG
     return (
         <View>
             <Text style={styles.headerText}>Color Picker Screen</Text>
             {/* Red colorPicker */}
-            <ColorPicker color="Red" topTitle="+ Red" bottomTitle="- Red" 
+            <ColorPicker color="Red" topTitle="+ Red" bottomTitle="- Red"
                 // onPressTop incrementing/controlling R state
-                onPressTop={() => { setRedIntensity(redIntensity + 1) }}
+                onPressTop={() => { colorValueController('Red', COLOR_CHANGE) }}
                 // onPressTop decrementing/controlling R state
-                onPressBottom={() => { setRedIntensity(redIntensity - 1) }}
+                onPressBottom={() => { colorValueController('Red', -1 * COLOR_CHANGE) }}
             />
             
-            <ColorPicker color="Green" topTitle="+ Green" bottomTitle="- Green" 
+            <ColorPicker color="Green" topTitle="+ Green" bottomTitle="- Green"
                 // onPressTop incrementing/controlling G state
-                onPressTop={() => {setGreenIntensity(greenIntensity + 1) }}
+                onPressTop={() => { colorValueController('Green', COLOR_CHANGE) }}
                 // onPressTop decrementing/controlling G state
-                onPressBottom={() => {setGreenIntensity(greenIntensity - 1) }}
+                onPressBottom={() => { colorValueController('Green', -1 * COLOR_CHANGE) }}
             />
             
             <ColorPicker color="Blue" topTitle="+ Blue" bottomTitle="- Blue" 
                 // onPressTop incrementing/controlling B state
-                onPressTop={() => {setBlueIntensity(blueIntensity + 1)}}
+                onPressTop={() => { colorValueController('Blue', COLOR_CHANGE) }}
                 // onPressTop decrementing/controlling B state
-                onPressBottom={() => {setBlueIntensity(blueIntensity - 1) }}
+                onPressBottom={() => { colorValueController('Blue', -1 * COLOR_CHANGE) }}
             />
+            <View style={{ minHeight: 200, minWidth: 200, backgroundColor: `rgb(${color})`}}></View>
+            
 
         </View>
-    )
+    )    
 }
 
 
