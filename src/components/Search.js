d8294c14bd455f9b97ef2a8a48d19c4544d71017
Joseph Pulaski <jpulaski79@gmail.com>
2020-03-18 23:19:26 -0400

    Setup onSearchCallApi method

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { TextInput, StyleSheet, Text, View } from 'react-native'
-import Feather  from 'react-native-vector-icons/dist/FontAwesome';
+import Feather from 'react-native-vector-icons/FontAwesome';
 
 
-const Search = ({title }) => {
+const Search = ({ placeholder, value, onChangeText, onEndEditing, onSearchCallApi}) => {
 
     return (
-        <View style={styles.searchView}>
-            <Feather size={30} name="search"/>
-            <Text> {title}</Text>
-            
-            <TextInput></TextInput>
+        <View style={styles.searchBox}>
+            {/* icon */}
+            <Feather style={{ alignSelf: 'center', marginHorizontal: 15}}size={35} name="search"/>
+            {/* Search Input */}
+            <TextInput 
+                autoCapitalize='none'
+                autoCorrect={false}
+                enablesReturnKeyAutomatically={true}
+                style={styles.input} 
+                value={value} 
+                placeholder={placeholder} 
+                onChangeText={onChangeText}
+                onEndEditing={onEndEditing}
+                onSearchCallApi={onSearchCallApi()}
+            />
         </View>
     )
 }
@@ -18,10 +28,16 @@ const Search = ({title }) => {
 export default Search
 
 const styles = StyleSheet.create({
-    searchView: {
-        backgroundColor: 'rgb(51,112,170)',
+    searchBox: {
+        flexDirection: 'row',
+        backgroundColor: 'rgb(210,210,210)',
         height: 50,
         margin: 15,
         borderRadius: 5,
+        
+    },
+    input: {
+        flex: 1,
+        fontSize: 20,
     }
 })
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Search from '../components/Search'
 
 const SearchScreen = () => {
+    const [searchText, handleSearchText] = useState('')
+
     return (
         <View>
-            <Search title="search bar"/>
+            {/* Search Bar */}
+            <Search 
+                value={searchText} 
+                placeholder="Search" 
+                onChangeText={
+                    newSearchText => {
+                        handleSearchText(newSearchText)}} 
+                title="search bar"
+                onEndEditing={() => {console.log(`${searchText} RETURN`)}}
+                onSearchCallApi={() => console.log(`${searchText}`)}
+                />
+            <Text>{searchText}</Text>
+            
         </View>
     )
 }
