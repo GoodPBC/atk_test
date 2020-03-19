cc754beeeb85adbe26c63a5aab39b8edcc8b96b0
Joseph Pulaski <jpulaski79@gmail.com>
2020-03-19 00:26:50 -0400

    axios working

---
+++ src/api/yelp.js
@@ -0,0 +1,26 @@
+// Yelp API library
+import axios from 'axios'
+
+
+/*
+YELP API DATA
+
+Client ID
+P5OJPnzLiuO3TGKCx14OPA
+API Key
+rK5ViTtJgH0Rd0NBvu5t7mEDU8LD7OP-SdFLZTp_mJlgy4unKthNbCPmnI4cWjo8FBVLPfRp5e7MXYvxLOMJuHKs4yCO9DHTuhf1HwYqCo4vvreB90E2HnBeMAlvXnYx
+
+
+GET REQUEST FOR YELP SEARCH 
+GET https://api.yelp.com/v3/businesses/search
+
+GET REQUEST FOR YELP BUSINESS SPECIFIC SEARCH 
+GET https://api.yelp.com/v3/businesses/{id}
+*/
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'Bearer rK5ViTtJgH0Rd0NBvu5t7mEDU8LD7OP-SdFLZTp_mJlgy4unKthNbCPmnI4cWjo8FBVLPfRp5e7MXYvxLOMJuHKs4yCO9DHTuhf1HwYqCo4vvreB90E2HnBeMAlvXnYx',
+    },
+});
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Search from '../components/Search'
+import yelp from '../api/yelp'
+
 
 const SearchScreen = () => {
     const [searchText, handleSearchText] = useState('')
+    const [yelpResults, setYelpResults] = useState([])
 
     // Axios Helper
-    const ApiHelper = () => {
+    const ApiHelper = async () => {
+        const res =  await yelp.get(`/search`, {
+            //api headers here
+            params: {
+                term: searchText,
+                limit: 10,
+                location: `jersey city`
+            }
+        });
+        console.log(res.data.businesses)
+        //pass results to useState[]
+        setYelpResults(
+            res.data.businesses
+        )
 
     }
 
@@ -18,12 +34,17 @@ const SearchScreen = () => {
                 placeholder="Search" 
                 onChangeText={
                     newSearchText => {
-                        handleSearchText(newSearchText)}} 
+                        handleSearchText(newSearchText)
+                    }
+                } 
                 title="search bar"
                 onEndEditing={() => {console.log(`${searchText} RETURN`)}}
                 onSearchSubmit={() => console.log(`${searchText} Was Submitted`)} 
+                onSearchSubmit={ApiHelper} 
                 />
             <Text>{searchText}</Text>
+
+            <Text>We found {yelpResults.length} results </Text>
             
         </View>
     )
